89754cc74d390b877d0b2990a9ad5e8da46ec8a4
Gennadii Fairushin <yozi-developer@yandex.ru>
2019-11-14 18:49:27 +0500

    cleanup

--- src/di/tokens.ts
+++ src/di/tokens.ts
@@ -3,4 +3,3 @@ import { App } from "../App/types";
 
 const AppToken = new Token<App>(Symbol("App"));
 type AppToken = TokenType<typeof AppToken>;
-
--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { testFn } from "./index";
-
-describe("test function", () => {
-  it("return 42 ", async function() {
-    const res = testFn();
-    expect(res).toBe(42);
-  });
-});
